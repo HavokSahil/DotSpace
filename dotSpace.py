8849ae505c1ee1e022e007229c7574841c62cb88
Sahil Raj <87008169+HavokSahil@users.noreply.github.com>
2024-02-01 18:08:25 +0530

    seperated entity as a module and added abstraction for light source

--- dotSpace.py
+++ dotSpace.py
@@ -1,89 +1,13 @@
 import numpy as np
 import math
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from time import sleep
 from scipy.interpolate import interp1d
+from Entity.surfaces import Donut, MbStrip
+
 
 import warnings
 warnings.filterwarnings("ignore", message="Conversion of an array with ndim > 0 to a scalar is deprecated", category=DeprecationWarning)
+  
 
-class Donut:
-    def __init__(self, nZ, nTheta, innerRadius, outerRadius):
-        self.name="Donut"
-
-        assert innerRadius<=outerRadius
-
-        self._innerRad = innerRadius
-        self._outerRad = outerRadius
-
-        self._nz = nZ
-        self._nTheta = nTheta
-        self._zRange = (outerRadius-innerRadius)/2
-        self.zPoints = np.linspace(-self._zRange, self._zRange, self._nz)
-
-        self.nPoints = 2*nZ*nTheta
-
-        # Coordinates of sampled points
-        self.coordinates = np.zeros(shape=(2*self._nz*self._nTheta, 3), dtype=np.float32)
-
-        # Gradients of each point
-        self.pointsGrad = np.zeros(shape=(2*self._nz*self._nTheta, 3), dtype=np.float64)
-
-        _counter = 0
-        for idx, point in enumerate(self.zPoints):
-            r1 = (self._innerRad+self._outerRad)/2 + math.sqrt((self._outerRad-self._innerRad)**2 - 4*(point**2))/2
-            r2 = (self._innerRad+self._outerRad)/2 - math.sqrt((self._outerRad-self._innerRad)**2 - 4*(point**2))/2
-
-            for idxT, angle in enumerate(np.linspace(0, 2*math.pi, self._nTheta)):
-
-                # Innner and Outer Circle
-                for r in [r1, r2]:
-                    if (_counter>=self.nPoints): break
-                    _x = r*math.cos(angle)
-                    _y = r*math.sin(angle)
-                    self.coordinates[_counter][0]=_x
-                    self.coordinates[_counter][1]=_y
-                    self.coordinates[_counter][2]=point
-                    self.pointsGrad[_counter] = self.getPointGradient(_x, _y, point)
-                    _counter+=1
-
-
-
-
-    def getPointGradient(self, _x, _y, _z):
-        grad = np.array([(2*_x - (self._innerRad+self._outerRad)*_x/math.dist([_x, _y], [0, 0])), (2*_y - 4*_y/math.dist([_x, _y], [0, 0])), 2*_z])
-        unitGrad = grad/np.linalg.norm(grad)
-        return unitGrad
-
-    def plotDonut(self):
-        fig = plt.figure(figsize=(7, 7), dpi=100)
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(self.coordinates[:, 0], self.coordinates[:, 1], self.coordinates[:, 2])
-        ax.set_title("Donut Points 3D plot")
-        ax.set_xlabel('X-axis')
-        ax.set_ylabel('Y-axis')
-        ax.set_zlabel('Z-axis')
-        ax.set_xlim(-self._outerRad-1, self._outerRad+1)
-        ax.set_ylim(-self._outerRad-1, self._outerRad+1)
-        ax.set_zlim(-self._outerRad-1, self._outerRad+1)
-        plt.show()
-
-    def getParams(self):
-        return {
-            "name": self.name,
-            "innerRadius": self._innerRad,
-            "outerRadius": self._outerRad,
-            "num_Z": self._nz,
-            "num_Theta": self._nTheta
-        } 
-    
-    def getCoordinates(self):
-        return self.coordinates
-    
-    def getAllGradients(self):
-        return self.pointsGrad
-    
 
 class Camera:
     def __init__(self, cx, cy, cz):
@@ -115,7 +39,6 @@ class Camera:
         R = np.linalg.inv(np.matmul(R1, R2))
         return R
     
-
     def setCameraCoordinates(self, coordinates):
         _m=(np.matmul(self.rotationMatrix, coordinates.T-self.position)).T
         _m[:, 0] = -1*_m[:, 0]
@@ -124,17 +47,36 @@ class Camera:
     def getCameraCoordinates(self):
         return self._cameraCoordinates
 
+    def focus(self, Object):
+        self._cameraCoordinates = None
+        self.setCameraCoordinates(Object.getCoordinates())
+
+
+class LightSource:
+    def __init__(self, lx, ly, lz):
+        self._vector = np.array([
+            [lx],
+            [ly],
+            [lz]
+        ])/np.linalg.norm([lx, ly, lz])
+    
+    def getLightVector(self):
+        return self._vector
+
+    def getSourceGradAlignment(self, Object):
+        return -np.dot(Object.getAllGradients(), self._vector)
+
 
 class Screen():
-    def __init__(self, reso, dist, i, j, k):
+    def __init__(self, reso, dist):
+        assert reso>0
         self.resolution = reso
+
         self._screen = np.zeros(shape=(reso, reso), dtype=np.float32)
-
         self._screenBuffer = None
-
         self._mask = np.zeros(shape=(reso, reso), dtype=np.float32)
         self._dist = dist
-        self.lightSource = np.array([i, j, k], dtype=np.float32)/np.linalg.norm([i, j, k])
+
         self.paintParams = {
             0.90: "@ ",
             0.80: "$ ",
@@ -148,35 +90,44 @@ class Screen():
             0: "  "
         }
 
-    def generateScreenBuffer(self, cameraCoorindates, grad):
-        self._screenBuffer = np.zeros(shape=(cameraCoorindates.shape[0], 4), dtype=np.float32)
+    # generates the position, z-index, point illumination for all points in the scene
+    
+    def clearToDefault(self):
+        self._screen = np.zeros(shape=(self.resolution, self.resolution), dtype=np.float32)
+        self._screenBuffer = None
+        self._mask = np.zeros(shape=(self.resolution, self.resolution), dtype=np.float32)
 
-        for idx, coordinates in enumerate(cameraCoorindates):
-            magnification_factor = 10
-            _x, _y, _z = coordinates.T[0], coordinates.T[1], coordinates.T[2]
-            _scaler = self._dist/_x
-            _y = _y*_scaler*magnification_factor
-            _z = _z*_scaler*magnification_factor
 
-            brightness = -np.dot(self.lightSource, grad[idx])
-            self._screenBuffer[idx][0]=_z
-            self._screenBuffer[idx][1]=_y
-            self._screenBuffer[idx][2]=_x
-            self._screenBuffer[idx][3]=brightness
+    def generateScreenBuffer(self, cameraCoordinates, alignmentVector):
+        self._screenBuffer = np.zeros(shape=(cameraCoordinates.shape[0], 4), dtype=np.float32)
+        magnification_factor = 10
+        _scaler = magnification_factor*(self._dist/np.dot(cameraCoordinates, np.array([
+            [1],
+            [0],
+            [0]
+        ])))
+        for idx, row in enumerate(_scaler):
+            scaled_position = cameraCoordinates[idx]*float(row[0])
+            self._screenBuffer[idx][0] = scaled_position.T[2]
+            self._screenBuffer[idx][1] = scaled_position.T[1]
+            self._screenBuffer[idx][2] = float(cameraCoordinates[idx].T[0])
+            self._screenBuffer[idx][3] = alignmentVector[idx]
 
     def getScreenBuffer(self):
         return self._screenBuffer
 
+    # for all possible points in the scene, map all points that lie within camera view
     def mapBufferOnScreen(self):
         _min1, _max1 = self._screenBuffer[:, 0].min(), self._screenBuffer[:, 0].max()
         _min2, _max2 = self._screenBuffer[:, 1].min(), self._screenBuffer[:, 1].max()
 
-        print(_min1, _max1, _min2, _max2)
-        interpolatorX = interp1d([_min1, _max1], [0, self.resolution-1])
-        interpolatorY = interp1d([_min2, _max2], [0, self.resolution-1])
+        minA = min(_min1, _min2)
+        maxA = max(_max1, _max2)
+        
+        interpolator = interp1d([minA, maxA], [0, self.resolution-1])
 
         for idx, item in enumerate(self._screenBuffer):
-            camX, camY = int(interpolatorX(item[0])), int(interpolatorY(item[1]))
+            camX, camY = int(interpolator(item[0])), int(interpolator(item[1]))
             if (item[2]>self._mask[camX][camY] and item[3]>self._screen[camX][camY]):
                 self._screen[camX][camY]=item[3]
 
@@ -184,12 +135,12 @@ class Screen():
     def paintScreen(self):
         string_screen = ""
         for idx_row, row in enumerate(self._screen):
+            string_screen+="\n"
             for idx_col, col in enumerate(row):
                 for params in self.paintParams.keys():
                     if col>=params:
                         string_screen+=self.paintParams[params]
                         break
-            string_screen+="\n"
             
         return string_screen
     
@@ -203,20 +154,28 @@ class Screen():
             
 
 
-def main():
-    donut = Donut(40, 60, 1, 3)
-
-    cameraVector = [0, 0, 10]
-
-    camera = Camera(cameraVector[0], cameraVector[1], cameraVector[2])
-    camera.setCameraCoordinates(donut.getCoordinates())
-    cameraCoordinates = camera.getCameraCoordinates()
-
-    canvas = Screen(50, 7, cameraVector[0], cameraVector[1], cameraVector[2])
-    canvas.generateScreenBuffer(cameraCoordinates, donut.getAllGradients())
+# Helper Function
+def focusAndPlot(Object, camera, canvas, lightSource):
+    camera.focus(Object)
+    canvas.generateScreenBuffer(camera.getCameraCoordinates(), lightSource.getSourceGradAlignment(Object))
     canvas.mapBufferOnScreen()
 
-    print(canvas.paintScreen())
+    paintedCanvas = canvas.paintScreen()
+    print(paintedCanvas, end='\r')
+
+
+def main():
+    donut = Donut(50, 60, 1, 3)
+    strip = MbStrip(4, 2, 50, 30)
+
+    cameraVector = [5, 0, 10]
+    lightSource = LightSource(cameraVector[0], cameraVector[1], cameraVector[2])
+    camera = Camera(cameraVector[0], cameraVector[1], cameraVector[2])
+    canvas = Screen(50, 7)
+    focusAndPlot(donut, camera, canvas, lightSource)
+    canvas.clearToDefault()
+    focusAndPlot(strip, camera, canvas, lightSource)
+
 
 if __name__=='__main__':
     main()
